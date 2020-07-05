d4f8197f8c341f5cb5d7b3a7948b9f8e7021d1ab
Israel-Laguan <israellaguan@gmail.com>
2020-07-05 16:38:02 -0500

    feat(TEST): add Jest configuration

---
+++ jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  collectCoverageFrom: [
+    'index.js',
+    '!node_modules/**',
+    'api/**/*.js',
+    'server/**/*.js',
+    'service/**/*.js',
+    'util/**/*.js'
+  ]
+}
