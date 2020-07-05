a5b2f2f843f7c772e6080f14b856d87fbd64031a
Israel-Laguan <israellaguan@gmail.com>
2020-07-05 18:25:36 -0500

    feat(TESTS): Add tests, update jest configuration and bump version  On branch dev  Changes to be committed: 	modified:   jest.config.js 	modified:   package.json 	renamed:    test/server.test.js -> test/api.test.js

--- jest.config.js
+++ jest.config.js
@@ -2,11 +2,8 @@ module.exports = {
   testEnvironment: 'node',
   testURL: 'http://localhost',
   collectCoverageFrom: [
-    'index.js',
     '!node_modules/**',
     'api/**/*.js',
-    'server/**/*.js',
-    'service/**/*.js',
-    'util/**/*.js'
+    'service/**/*.js'
   ]
 }
